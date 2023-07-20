5351dae735763e2c57f5ae260d5dd3e098bc3439
Jhonata <gutemberg.jhonata@gmail.com>
2023-07-20 08:33:39 -0300

    feat: mapeando entidade proprietario

--- src/main/java/com/gutinhotech/gutotransito/domain/model/Proprietario.java
+++ src/main/java/com/gutinhotech/gutotransito/domain/model/Proprietario.java
@@ -1,12 +1,23 @@
 package com.gutinhotech.gutotransito.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+//@Table(name = "proprietario")
 public class Proprietario {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
