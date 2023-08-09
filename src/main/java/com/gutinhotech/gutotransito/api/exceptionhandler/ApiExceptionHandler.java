2645ced72990c0d94764b918f19b4d7728d0b7ee
Jhonata <gutemberg.jhonata@gmail.com>
2023-08-09 16:39:18 -0300

    customizando resposta da API

--- src/main/java/com/gutinhotech/gutotransito/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/gutinhotech/gutotransito/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,18 +1,45 @@
 package com.gutinhotech.gutotransito.api.exceptionhandler;
 
+import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.gutinhotech.gutotransito.domain.exception.NegocioException;
 
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("https://algatransito.com/errors/campos-invalidos"));
+        Map<String, String> fields = ex.getBindingResult()
+            .getAllErrors()
+            .stream()
+            .collect(Collectors.toMap(objectError -> ((FieldError) objectError).getField(), 
+                ObjectError::getDefaultMessage));
+        problemDetail.setProperty("fields", fields);
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
 
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> capturar(NegocioException e) {
         return ResponseEntity.badRequest()
             .body(e.getMessage());
     }
-    
+
 }
