4a861440dfa2d06cd6e230db3a9df3001296a602
Jhonata <gutemberg.jhonata@gmail.com>
2023-08-09 09:46:36 -0300

    implementando exception handler

--- src/main/java/com/gutinhotech/gutotransito/api/controller/ProprietarioController.java
+++ src/main/java/com/gutinhotech/gutotransito/api/controller/ProprietarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gutinhotech.gutotransito.domain.exception.NegocioException;
 import com.gutinhotech.gutotransito.domain.model.Proprietario;
 import com.gutinhotech.gutotransito.domain.repository.ProprietarioRepository;
 import com.gutinhotech.gutotransito.domain.service.RegistroProprietarioService;
@@ -79,6 +81,12 @@ public class ProprietarioController {
         }
         registroProprietarioService.excluir(proprietarioId);
         return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<String> capturar(NegocioException e) {
+        return ResponseEntity.badRequest()
+            .body(e.getMessage());
     }
 
 }
