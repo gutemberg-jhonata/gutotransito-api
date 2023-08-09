b639e5c13007cec3d5384947993689d3b44309d8
Jhonata <gutemberg.jhonata@gmail.com>
2023-08-09 10:47:08 -0300

    implementando modelo de Veiculo

---
+++ src/main/java/com/gutinhotech/gutotransito/domain/model/Veiculo.java
@@ -0,0 +1,62 @@
+package com.gutinhotech.gutotransito.domain.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Veiculo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotNull
+    @ManyToOne
+    //@JoinColumn(name = "proprietario_id")
+    private Proprietario proprietario;
+
+    @NotBlank
+    @Size(max = 20)
+    private String marca;
+
+    @NotBlank
+    @Size(max = 20)
+    private String modelo;
+
+    @NotBlank
+    @Size(max = 7)
+    private String placa;
+
+    @NotBlank
+    @Size(max = 20)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @NotNull
+    private LocalDateTime dataCadastro;
+
+    @NotNull
+    private LocalDateTime dataApreensao;
+
+    public enum Status {
+        REGULAR, APREENDIDO
+    }
+
+}
