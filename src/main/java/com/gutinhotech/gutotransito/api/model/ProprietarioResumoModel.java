36f15566d77047bbfc1d0a0ae13dac6aa636c56d
Jhonata <gutemberg.jhonata@gmail.com>
2023-08-10 13:12:33 -0300

    compondo objetos no representation model

---
+++ src/main/java/com/gutinhotech/gutotransito/api/model/ProprietarioResumoModel.java
@@ -0,0 +1,13 @@
+package com.gutinhotech.gutotransito.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProprietarioResumoModel {
+
+    private Long id;
+    private String nome;
+    
+}
